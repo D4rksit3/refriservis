4f775ec02cb49c2edffddb42c73cab831e596568
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-09-17 12:50:54 -0500

    s

--- operador/reporte.php
+++ operador/reporte.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once __DIR__.'/../config/db.php';
 require_once __DIR__.'/../lib/fpdf.php';
