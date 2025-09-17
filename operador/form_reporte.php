9aa92a9c6a4494f0b9206cb7457dd3cd7d61b7c4
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-09-17 11:48:35 -0500

    i

--- operador/form_reporte.php
+++ operador/form_reporte.php
@@ -1,5 +1,11 @@
 <?php
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
 session_start();
 require_once __DIR__.'/../config/db.php';
 
