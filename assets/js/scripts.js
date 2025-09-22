f96b6996174a75e4bdc9ded672d73b9b14fae5b5
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-09-22 13:28:28 -0500

    a

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,48 +1,51 @@
 $(document).ready(function () {
-    // Inicializar DataTable solo UNA vez
-    var tabla = $('#tablaEquipos').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: {
-            url: 'equipos_data.php',
-            type: 'GET'
-        },
-        pageLength: 10,
-        lengthMenu: [10, 25, 50, 100],
-        columns: [
-            { data: 'id_equipo' },
-            { data: 'Nombre' },
-            { data: 'Descripcion' },
-            { data: 'Cliente' },
-            { data: 'Categoria' },
-            { data: 'Estatus' },
-            { data: 'Fecha_validad' },
-            { data: 'acciones', orderable: false, searchable: false }
-        ],
-        language: {
-            url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
-        }
-    });
+    // Inicializar DataTable solo una vez
+    if (!$.fn.DataTable.isDataTable('#tablaEquipos')) {
+        var tabla = $('#tablaEquipos').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: 'equipos_data.php',
+                type: 'GET'
+            },
+            pageLength: 10,
+            lengthMenu: [10, 25, 50, 100],
+            columns: [
+                { data: 'id_equipo' },
+                { data: 'Nombre' },
+                { data: 'Descripcion' },
+                { data: 'Cliente' },
+                { data: 'Categoria' },
+                { data: 'Estatus' },
+                { data: 'Fecha_validad' },
+                { data: 'acciones', orderable: false, searchable: false }
+            ],
+            language: {
+                url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
+            }
+        });
+    }
 
-    // Delegación de eventos para botones dentro de DataTable
-    $('#tablaEquipos tbody').on('click', '.editar', function () {
-        var rowData = tabla.row($(this).parents('tr')).data();
+    // Editar
+    $(document).on('click', '.editar', function () {
+        var row = $(this).closest('tr');
+        var data = tabla.row(row).data();
 
-        $('#editId').val(rowData.id_equipo);
-        $('#editNombre').val(rowData.Nombre);
-        $('#editDescripcion').val(rowData.Descripcion);
-        $('#editCliente').val(rowData.Cliente);
-        $('#editCategoria').val(rowData.Categoria);
-        $('#editEstatus').val(rowData.Estatus);
-        $('#editFecha').val(rowData.Fecha_validad);
-
+        $('#editId').val(data.id_equipo);
+        $('#editNombre').val(data.Nombre);
+        $('#editDescripcion').val(data.Descripcion);
+        $('#editCliente').val(data.Cliente);
+        $('#editCategoria').val(data.Categoria);
+        $('#editEstatus').val(data.Estatus);
+        $('#editFecha').val(data.Fecha_validad);
         $('#modalEditar').modal('show');
     });
 
-    $('#tablaEquipos tbody').on('click', '.eliminar', function () {
-        var rowData = tabla.row($(this).parents('tr')).data();
-
-        $('#deleteId').val(rowData.id_equipo);
+    // Eliminar
+    $(document).on('click', '.eliminar', function () {
+        var row = $(this).closest('tr');
+        var data = tabla.row(row).data();
+        $('#deleteId').val(data.id_equipo);
         $('#modalEliminar').modal('show');
     });
 });
