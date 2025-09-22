527b3464aede56f5460df0b1f7b8ba16c71cd5ea
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-09-22 09:25:40 -0500

    a

--- includes/footer.php
+++ includes/footer.php
@@ -8,6 +8,15 @@
   </div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- DataTables -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+
+<!-- DataTables CSS (por si falta) -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css"/>
+
 <script src="/assets/js/scripts.js"></script>
 </body>
 </html>
