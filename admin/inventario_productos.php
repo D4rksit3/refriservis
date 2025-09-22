b0b2b8efb193d2ee65c44f8f96668c1cf44d38ad
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-09-22 14:33:59 -0500

    s

--- admin/inventario_productos.php
+++ admin/inventario_productos.php
@@ -133,6 +133,7 @@ require_once __DIR__.'/../includes/header.php';
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 
 <?php include __DIR__ . '/../includes/footer.php'; ?>
