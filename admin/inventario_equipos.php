39b1461764d687a770e3674185e6077157cd8506
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-09-22 09:23:16 -0500

    a

--- admin/inventario_equipos.php
+++ admin/inventario_equipos.php
@@ -167,19 +167,19 @@ const tabla = new DataTable('#tablaEquipos', {
     pageLength: 10,
     columns: [
         { data: "id_equipo" },
-        { data: "Nombre" },
-        { data: "Descripcion" },
-        { data: "Cliente" },
-        { data: "Categoria" },
-        { data: "Estatus" },
-        { data: "Fecha_validad" },
+        { data: "nombre" },
+        { data: "descripcion" },
+        { data: "cliente" },
+        { data: "categoria" },
+        { data: "estatus" },
+        { data: "fecha_validad" },
         { data: null, render: function (data) {
             return `
-              <form method="post" style="display:inline-block">
+            <form method="post" style="display:inline-block">
                 <input type="hidden" name="accion" value="eliminar">
                 <input type="hidden" name="id_equipo" value="${data.id_equipo}">
                 <button type="submit" class="btn btn-danger btn-sm">🗑️</button>
-              </form>`;
+            </form>`;
         }}
     ],
     language: { url: "//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json" }
