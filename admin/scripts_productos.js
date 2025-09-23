f18e347c8da8d920118347aa898fdd6ab526019e
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-09-23 09:00:28 -0500

    a

--- admin/scripts_productos.js
+++ admin/scripts_productos.js
@@ -12,50 +12,44 @@ $(document).ready(function(){
             {data:'Valor_unitario'},
             {data:'acciones', orderable:false, searchable:false}
         ],
-        columnDefs:[{
-            targets:5,
-            render:function(data,type,row){
-                return data; // renderizar HTML
-            }
-        }],
         language:{ url:'//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json' }
     });
 
-    // AGREGAR
+    // -------- FUNCION CERRAR MODAL + LIMPIEZA --------
+    function cerrarModal(modalId) {
+        var modal = bootstrap.Modal.getInstance(document.getElementById(modalId));
+        if(modal){ modal.hide(); }
+        $('body').removeClass('modal-open');
+        $('.modal-backdrop').remove();
+        $('body').css('overflow', 'auto');
+    }
+
+    // -------- AGREGAR --------
     $('#formAgregarEquipo').on('submit', function(e){
         e.preventDefault();
 
         $.post('equipos_crud.php', $(this).serialize(), function(resp){
             if(resp.success){
-                // Quitar el foco antes de cerrar
-                document.activeElement.blur();
-
-                // Resetear form
                 $('#formAgregarEquipo')[0].reset();
-
-                // Cerrar modal de forma segura
-                $('#modalAgregar').one('hidden.bs.modal', function(){
-                    $('#tablaEquipos').DataTable().ajax.reload();
-                }).modal('hide');
-
+                tabla.ajax.reload(null,false);
+                cerrarModal('modalAgregar');
             } else {
                 alert('Error al agregar');
             }
         }, 'json');
     });
 
-
-    // EDITAR - abrir modal con eventos delegados
+    // -------- EDITAR (abrir modal) --------
     $('#tablaProductos').on('click', '.editar', function(){
         var id = $(this).data('id');
-        $.getJSON('productos_data.php', function(resp){
-            var p = resp.data.find(x => x.productos_id == id);
+        $.getJSON('productos_crud.php', { action:'get', id:id }, function(p){
             if(p){
                 $('#editId').val(p.productos_id);
                 $('#editNombre').val(p.Nombre);
                 $('#editCategoria').val(p.Categoria);
                 $('#editEstatus').val(p.Estatus);
                 $('#editValor').val(p.Valor_unitario);
+
                 var modal = new bootstrap.Modal(document.getElementById('modalEditar'));
                 modal.show();
             }
@@ -64,14 +58,13 @@ $(document).ready(function(){
 
     $('#formEditar').submit(function(e){
         e.preventDefault();
-        $.post('productos_crud.php', $(this).serialize(), function(){
+        $.post('productos_crud.php', $(this).serialize(), function(resp){
             tabla.ajax.reload(null,false);
-            var modal = bootstrap.Modal.getInstance(document.getElementById('modalEditar'));
-            modal.hide();
-        });
+            cerrarModal('modalEditar');
+        }, 'json');
     });
 
-    // ELIMINAR
+    // -------- ELIMINAR --------
     $('#tablaProductos').on('click', '.eliminar', function(){
         var id = $(this).data('id');
         $('#deleteId').val(id);
@@ -81,11 +74,10 @@ $(document).ready(function(){
 
     $('#formEliminar').submit(function(e){
         e.preventDefault();
-        $.post('productos_crud.php', $(this).serialize(), function(){
+        $.post('productos_crud.php', $(this).serialize(), function(resp){
             tabla.ajax.reload(null,false);
-            var modal = bootstrap.Modal.getInstance(document.getElementById('modalEliminar'));
-            modal.hide();
-        });
+            cerrarModal('modalEliminar');
+        }, 'json');
     });
 
 });
