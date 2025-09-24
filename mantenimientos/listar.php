d3d242b9e29191c964208b0d256e05e173f202fd
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-09-24 08:47:06 -0500

    s

--- mantenimientos/listar.php
+++ mantenimientos/listar.php
@@ -160,6 +160,7 @@ require_once __DIR__.'/../includes/header.php';
     </div>
   </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <script>
 let pagina = 1;
