50f6366e7648ff9ccb8adbe743c2c79f436c8a93
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-10-01 08:51:36 -0500

    s

--- operador/mis_mantenimientos.php
+++ operador/mis_mantenimientos.php
@@ -63,7 +63,6 @@ $stmtTotal->execute([$_SESSION['usuario_id']]);
 $totalRegistros = $stmtTotal->fetchColumn();
 $totalPaginas = ceil($totalRegistros / $itemsPorPagina);
 
-
 // 📌 Mapa de categorías → formularios (ver/generar)
 $mapaReportes = [
     'VRV - FORMATO DE CALIDAD'                            => '/operador/reportes/vrv.php',
@@ -89,11 +88,6 @@ $mapaDescargas = [
     'BOMBAS DE AGUA - FORMATO DE CALIDAD'                 => '/operador/reportes/guardar_reporte_bombas.php',
     'REPORTE DE SERVICIO TECNICO'                         => '/operador/reportes/guardar_reporte_servicio.php'
 ];
-
-$urlReporte  = $mapaReportes[$categoria] ?? '/operador/reportes/servicio.php';
-$urlDescarga = ($mapaDescargas[$categoria] ?? '/operador/reportes/guardar_reporte_servicio.php') . '?id=' . $r['id'];
-
-
 ?>
 
 <div class="container my-3">
@@ -102,65 +96,52 @@ $urlDescarga = ($mapaDescargas[$categoria] ?? '/operador/reportes/guardar_report
   <?php if ($rows): ?>
     <div class="row">
       <?php foreach($rows as $r): ?>
+        <?php
+            $categoria = $r['categoria'] ?? '';
+            $id        = $r['id'] ?? 0;
+
+            // Resolver URLs según categoría
+            $urlReporte  = $mapaReportes[$categoria] ?? '/operador/form_reporte.php';
+            $urlDescarga = $mapaDescargas[$categoria] ?? '/operador/reportes/guardar_reporte_servicio.php';
+            $urlDescarga .= '?id=' . $id;
+        ?>
         <div class="col-md-6 mb-3">
           <div class="card shadow-sm h-100 border-0">
             <div class="card-body">
               <h6 class="card-title text-primary"><?= htmlspecialchars($r['titulo']) ?></h6>
-              <p class="card-text mb-1"><b>Categoría:</b> <?= htmlspecialchars($r['categoria'] ?? '-') ?></p>
+              <p class="card-text mb-1"><b>Categoría:</b> <?= htmlspecialchars($categoria) ?></p>
               <p class="card-text mb-1"><b>Cliente:</b> <?= htmlspecialchars($r['cliente'] ?? '-') ?></p>
-              
               <p class="card-text mb-1"><b>Digitador:</b> <?= htmlspecialchars($r['digitador'] ?? '-') ?></p>
-              <p class="card-text mb-1"><b>Fecha:</b> <?= $r['fecha'] ?></p>
-              <p class="card-text mb-2"><b>Estado:</b> <?= ucfirst($r['estado']) ?></p>
+              <p class="card-text mb-1"><b>Fecha:</b> <?= htmlspecialchars($r['fecha'] ?? '-') ?></p>
+              <p class="card-text mb-2"><b>Estado:</b> <?= ucfirst(htmlspecialchars($r['estado'] ?? '-')) ?></p>
 
               <div class="d-flex gap-2">
-                 <?php
-                  $categoria   = $r['categoria'];
-                  $urlReporte  = $mapaReportes[$categoria] ?? '/operador/reportes/servicio.php';
-                  $urlDescarga = ($mapaDescargas[$categoria] ?? '/operador/reportes/guardar_reporte_servicio.php') . '?id=' . $r['id'];
-                ?>
+                 <?php if (!empty($r['reporte_generado']) && $r['estado'] !== 'finalizado'): ?>
+                        <a href="<?= $urlReporte ?>?id=<?= $id ?>" 
+                           class="btn btn-secondary btn-sm w-100">Ver / Editar Reporte</a>
 
-                <?php if ($r['reporte_generado'] && $r['estado'] !== 'finalizado'): ?>
-                    <!-- Ver / Editar -->
-                    <a href="<?= $urlReporte ?>?id=<?= $r['id'] ?>" 
-                      class="btn btn-secondary btn-sm w-100">Ver / Editar Reporte</a>
+                 <?php elseif ($r['estado'] === 'pendiente' || $r['estado'] === 'en proceso'): ?>
+                        <a href="<?= $urlReporte ?>?id=<?= $id ?>" 
+                           class="btn btn-outline-success btn-sm w-100">Generar Reporte</a>
 
-                <?php elseif ($r['estado'] === 'pendiente' || $r['estado'] === 'en proceso'): ?>
-                    <!-- Generar -->
-                    <a href="<?= $urlReporte ?>?id=<?= $r['id'] ?>" 
-                      class="btn btn-outline-success btn-sm w-100">Generar Reporte</a>
-
-                <?php elseif ($r['estado'] === 'finalizado'): ?>
-                    <!-- Descargar -->
-                    <a href="<?= $urlDescarga ?>" 
-                      class="btn btn-primary btn-sm w-100" target="_blank">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" 
-                            fill="currentColor" class="bi bi-cloud-arrow-down" 
-                            viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M7.646 10.854a.5.5 
-                        0 0 0 .708 0l2-2a.5.5 
-                        0 0 0-.708-.708L8.5 
-                        9.293V5.5a.5.5 0 0 
-                        0-1 0v3.793L6.354 8.146a.5.5 
-                        0 1 0-.708.708z"/>
-                        <path d="M4.406 3.342A5.53 
-                        5.53 0 0 1 8 2c2.69 0 
-                        4.923 2 5.166 4.579C14.758 
-                        6.804 16 8.137 16 9.773 
-                        16 11.569 14.502 13 12.687 
-                        13H3.781C1.708 13 0 11.366 
-                        0 9.318c0-1.763 1.266-3.223 
-                        2.942-3.593.143-.863.698-1.723 
-                        1.464-2.383m.653.757c-.757.653-1.153 
-                        1.44-1.153 2.056v.448l-.445.049C2.064 
-                        6.805 1 7.952 1 9.318 1 10.785 2.23 
-                        12 3.781 12h8.906C13.98 12 15 10.988 
-                        15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 
-                        4.825 10.328 3 8 3a4.53 4.53 
-                        0 0 0-2.941 1.1z"/>
-                      </svg> Descargar Reporte
-                    </a>
-                <?php endif; ?>
+                 <?php elseif ($r['estado'] === 'finalizado'): ?>
+                        <a href="<?= $urlDescarga ?>" 
+                           class="btn btn-primary btn-sm w-100" target="_blank">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-arrow-down" viewBox="0 0 16 16">
+                              <path fill-rule="evenodd" d="M7.646 10.854a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 9.293V5.5a.5.5 0 0 0-1 0v3.793L6.354 8.146a.5.5 0 1 0-.708.708z"/>
+                              <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 
+                                      16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 
+                                      1.266-3.223 2.942-3.593.143-.863.698-1.723 
+                                      1.464-2.383m.653.757c-.757.653-1.153 
+                                      1.44-1.153 2.056v.448l-.445.049C2.064 
+                                      6.805 1 7.952 1 9.318 1 10.785 2.23 
+                                      12 3.781 12h8.906C13.98 12 15 10.988 
+                                      15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5
+                                      C12.188 4.825 10.328 3 8 3a4.53 4.53 
+                                      0 0 0-2.941 1.1z"/>
+                           </svg> Descargar Reporte
+                        </a>
+                 <?php endif; ?>
               </div>
             </div>
           </div>
