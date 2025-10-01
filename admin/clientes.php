fd572fe9b8859160e33c4d6f99eb54bdbc84f5d7
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-10-01 16:05:53 -0500

    s

--- admin/clientes.php
+++ admin/clientes.php
@@ -46,8 +46,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 // ELIMINAR CLIENTE
 // =========================
 if ($action === 'delete' && isset($_GET['id'])) {
-    $pdo->prepare('DELETE FROM clientes WHERE id=?')->execute([(int)$_GET['id']]);
-    header('Location: /admin/clientes.php?ok=1'); exit;
+    $id = (int)$_GET['id'];
+    if($id>0){
+        $stmt = $pdo->prepare('DELETE FROM clientes WHERE id=?');
+        $stmt->execute([$id]);
+    }
+    header('Location: clientes.php?ok=1');
+    exit;
+}
+
+if ($action === 'delete' && $_SERVER['REQUEST_METHOD']==='POST' && isset($_POST['id'])){
+    $id = (int)$_POST['id'];
+    $pdo->prepare('DELETE FROM clientes WHERE id=?')->execute([$id]);
+    header('Location: clientes.php?ok=1');
+    exit;
 }
 
 // =========================
@@ -108,7 +120,16 @@ if ($action === 'list') {
                             </td>
                             <td class="text-end">
                                 <a class="btn btn-sm btn-outline-primary" href="/admin/clientes.php?action=edit&id=<?=$c['id']?>">Editar</a>
-                                <a class="btn btn-sm btn-outline-danger" href="/admin/clientes.php?action=delete&id=<?=$c['id']?>" onclick="return confirm('Eliminar cliente?')">Eliminar</a>
+                                
+                               <!--  <a class="btn btn-sm btn-outline-danger" href="/admin/clientes.php?action=delete&id=<?=$c['id']?>" onclick="return confirm('Eliminar cliente?')">Eliminar</a>
+                             --><form method="post" action="clientes.php?action=delete" style="display:inline-block;" onsubmit="return confirm('Eliminar cliente?')">
+                                    <input type="hidden" name="id" value="<?=$c['id']?>">
+                                    <button class="btn btn-sm btn-outline-danger">Eliminar</button>
+                                </form>
+
+                            
+                            
+                            
                             </td>
                         </tr>
                     <?php endforeach; ?>
