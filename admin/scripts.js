0967353e724dff44e0942cd5d4b2c78ecf25ac8d
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-10-03 11:55:17 -0500

    D

--- admin/scripts.js
+++ admin/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     // ---------- TABLE: EQUIPOS ----------
     var tablaEquipos = $('#tablaEquipos').length ? $('#tablaEquipos').DataTable({
         processing: true,
-        serverSide: true,
+       /*  serverSide: true, */
         ajax: 'equipos_data.php',
         columns: [
             {data:'id_equipo'},
