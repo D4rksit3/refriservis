0e48e818fdc67a0e44bf73715aed09cedd5704e8
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-10-07 13:28:00 -0500

    w

--- admin/inventario_equipos.php
+++ admin/inventario_equipos.php
@@ -136,13 +136,11 @@ require_once __DIR__.'/../includes/header.php';
   </div>
 </div>
 
-<!-- SCRIPTS: jQuery, DataTables, Bootstrap 5, scripts.js (unificado) -->
+<!-- CORRECTO -->
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="scripts.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
 
 <?php include __DIR__ . '/../includes/footer.php'; ?>
