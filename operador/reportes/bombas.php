91391ef09d0f4f9c5990537047dd336d8bf8f021
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-10-10 18:28:45 -0500

    d

--- operador/reportes/bombas.php
+++ operador/reportes/bombas.php
@@ -532,6 +532,33 @@ $(document).ready(function(){
       }
     });
   });
+
+    $('#formAgregarEquipo').on('submit', function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      url: '/../../admin/equipos_crud.php', // archivo PHP que guarda el equipo
+      type: 'POST',
+      data: $(this).serialize(),
+      success: function(resp) {
+        try {
+          const r = JSON.parse(resp);
+          if (r.success) {
+            alert('✅ Equipo agregado correctamente');
+            $('#modalAgregarEquipo').modal('hide');
+            $('#formAgregarEquipo')[0].reset();
+          } else {
+            alert('⚠️ Error: ' + (r.message || 'No se pudo agregar.'));
+          }
+        } catch {
+          console.log(resp);
+          alert('Error inesperado');
+        }
+      }
+    });
+  });
+
+
 });
 
 
@@ -638,32 +665,9 @@ document.getElementById('formReporte').addEventListener('submit', function(e) {
   document.getElementById('observacionesFinal').value = JSON.stringify(data, null, 2);
 });
 
-$(document).ready(function() {
-  $('#formAgregarEquipo').on('submit', function(e) {
-    e.preventDefault();
 
-    $.ajax({
-      url: '/../../admin/equipos_crud.php', // archivo PHP que guarda el equipo
-      type: 'POST',
-      data: $(this).serialize(),
-      success: function(resp) {
-        try {
-          const r = JSON.parse(resp);
-          if (r.success) {
-            alert('✅ Equipo agregado correctamente');
-            $('#modalAgregarEquipo').modal('hide');
-            $('#formAgregarEquipo')[0].reset();
-          } else {
-            alert('⚠️ Error: ' + (r.message || 'No se pudo agregar.'));
-          }
-        } catch {
-          console.log(resp);
-          alert('Error inesperado');
-        }
-      }
-    });
-  });
-});
+
+
 
 
 </script>
