4733275a06dcffd5c1fdee0f4050dd5c94bdfd3b
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-10-17 09:13:09 -0500

    s

--- operador/reportes/guardar_reporte_servicio.php
+++ operador/reportes/guardar_reporte_servicio.php
@@ -314,7 +314,7 @@ function generarPDF(PDO $pdo, int $id) {
     $pdf->Ln(2);
 
     // ---------- OBSERVACIONES ----------
-   $pdf->AddPage();
+   /* $pdf->AddPage(); */
     /* $pdf->SetFont('Arial','B',10);
     $pdf->Cell(0,8, utf8_decode("Observaciones y Recomendaciones:"), 0, 1, 'L');
     $pdf->Ln(2); */
