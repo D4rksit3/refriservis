f58ecf0b02245609f0f09a10215a1fd6a3e94371
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-10-21 14:41:47 -0500

    s

--- asistencia/index.php
+++ asistencia/index.php
@@ -117,7 +117,7 @@
 
   <div class="container-form">
     <div class="brand">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Logo-placeholder.svg" alt="Logo">
+      <img src="logo.jpeg" alt="Logo">
     </div>
 
     <h4>Registro de Asistencia</h4>
