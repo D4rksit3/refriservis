aad838b7470b8fdcaf84f85d9d3ef482d90db98e
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-10-27 10:29:28 -0500

    s

--- operador/mis_mantenimientos.php
+++ operador/mis_mantenimientos.php
@@ -5,7 +5,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if (!isset($_SESSION['usuario']) || $_SESSION['rol'] !== 'operador' || $_SESSION['rol'] !== 'digitador') {
+if (
+    !isset($_SESSION['usuario']) ||
+    ($_SESSION['rol'] !== 'operador' && $_SESSION['rol'] !== 'digitador')
+) {
     header('Location: /index.php');
     exit;
 }
