3fa99e1f6b6a189e6e34c35feb2abc4d36740308
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-10-29 09:31:38 -0500

    s

--- mantenimientos/listar.php
+++ mantenimientos/listar.php
@@ -278,6 +278,7 @@ require_once __DIR__.'/../includes/header.php';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/js/bootstrap-select.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 
 <script>
 let pagina = 1;
