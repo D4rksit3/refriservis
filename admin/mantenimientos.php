7d890c4b736d083d28cd003e275f52e6ae92995e
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2025-10-29 09:45:16 -0500

    s

--- admin/mantenimientos.php
+++ admin/mantenimientos.php
@@ -264,26 +264,7 @@ function cargarMantenimientos() {
 }
 
 
-// Eliminar mantenimiento
-$('#tabla-mantenimientos').on('click', '.btn-eliminar', function(){
-  const id = $(this).data('id');
-  if(confirm('¿Seguro que deseas eliminar este mantenimiento? Esta acción no se puede deshacer.')) {
-    $.ajax({
-      url: '/mantenimientos/eliminar.php',
-      type: 'POST',
-      data: { id },
-      dataType: 'json',
-      success: function(res){
-        if(res.success){
-          alert('Mantenimiento eliminado correctamente');
-          cargarMantenimientos();
-        } else {
-          alert(res.error || 'Error al eliminar el mantenimiento');
-        }
-      }
-    });
-  }
-});
+
 
 
 // Abrir modal editar
